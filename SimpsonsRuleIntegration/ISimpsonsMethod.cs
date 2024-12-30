b7eda44d7241dba5a9306984c277ec60d4abb4bc
Patti Anderson <patti@scotnpatti.com>
2024-12-30 14:31:14 -0500

    Add project files.

---
+++ SimpsonsRuleIntegration/ISimpsonsMethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpsonsRuleIntegration
+{
+    public interface ISimpsonsMethod
+    {
+       /// <summary>
+       /// Impelement the Composite Simpson's Rule. 
+       /// Hard code the function. Then prompt the user 
+       /// for a, b, the maximum allowable error, and the 
+       /// maximum of the absolute value of the 
+       /// fourth derivative. 
+       /// </summary>
+       /// <param name="a"></param>
+       /// <param name="b"></param>
+       /// <param name="f"></param>
+       /// <param name="MaxError"></param>
+       /// <param name="MaxFourthDerivative"></param>
+       /// <returns></returns>
+        double Simpsons(double a, double b, Func<double, double> f, double MaxError, double MaxFourthDerivative);
+    }
+}
